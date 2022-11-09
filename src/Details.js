f0ad1e30ef096d936712987a86d9fcabf08910d1
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-09 16:10:50 +0200

    removing modals

--- src/Details.js
+++ src/Details.js
@@ -3,11 +3,13 @@ import { useParams } from 'react-router-dom';
 
 import { Carousel } from './Carousel';
 import { ErrorBoundary } from './ErrorBoundary';
+import { Modal } from './Modal';
 import { ThemeContext } from './ThemeContext';
 
 class Details extends Component {
   state = {
     loading: true,
+    showModal: false,
   };
 
   async componentDidMount() {
@@ -19,12 +21,14 @@ class Details extends Component {
     this.setState({ loading: false, ...json.pets[0] });
   }
 
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
   render() {
     if (this.state.loading) {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -37,10 +41,28 @@ class Details extends Component {
           </h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <a href="https://bit.ly/pet-adopt" target="_blank" rel="noreferrer">
+                    Yes
+                  </a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
