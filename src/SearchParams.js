f81c2e41e6434aa64acdd5136c820cec601a098e
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-09 08:58:29 +0200

    handling user input

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -30,7 +30,12 @@ export const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
