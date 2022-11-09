f423dda0c32198a460b3f7a10a03e58e6a0f59cb
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-09 14:28:19 +0200

    handle events in class components

--- src/Carousel.js
+++ src/Carousel.js
@@ -9,6 +9,12 @@ export class Carousel extends Component {
     images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: Number.parseInt(event.target.dataset.index, 10),
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -18,12 +24,15 @@ export class Carousel extends Component {
         <img src={images[active]} alt="animal" width="400" height="400" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               src={photo}
               alt="animal thumbnail"
               className={index === active ? 'active' : ''}
+              data-index={index}
               width="100"
               height="100"
+              onClick={this.handleIndexClick}
               key={photo}
             />
           ))}
