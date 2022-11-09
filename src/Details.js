50e8a97ce8f378c0dd53c676e7aea54ffea740bc
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-09 13:24:53 +0200

    class components

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,40 @@
-import { useParams } from 'react-router-dom';
+import { Component } from 'react';
+// import { useParams } from 'react-router-dom';
 
-export const Details = () => {
-  const { id } = useParams();
+export class Details extends Component {
+  constructor(props) {
+    super(props);
 
-  return <h2>{id}</h2>;
-};
+    this.state = {
+      loading: true,
+    };
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.props;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+// Functional component example:
+// export const Details = () => {
+//   const { id } = useParams();
+//
+//   return <h2>{id}</h2>;
+// };
