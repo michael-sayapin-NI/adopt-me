ccac2a8e336c70dc9cd7cb0de6c48ab3b2199c0c
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-09 14:18:51 +0200

    managing state in class components

--- src/Carousel.js
+++ src/Carousel.js
@@ -8,4 +8,27 @@ export class Carousel extends Component {
   static defaultProps = {
     images: ['http://pets-images.dev-apis.com/pets/none.jpg'],
   };
+
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" width="400" height="400" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              src={photo}
+              alt="animal thumbnail"
+              className={index === active ? 'active' : ''}
+              width="100"
+              height="100"
+              key={photo}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { Carousel } from './Carousel';
+
 class Details extends Component {
   state = {
     loading: true,
@@ -20,10 +22,12 @@ class Details extends Component {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
