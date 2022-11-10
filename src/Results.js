f7b41dcc785746724d7f3af7e6c563f52354d512
Michael Sayapin <michael.sayapin@naturalint.com>
2022-11-10 12:32:19 +0200

    grid and breakpoints

--- src/Results.js
+++ src/Results.js
@@ -9,7 +9,7 @@ export const Results = ({ pets }) => {
   const isPetsEmpty = pets.length === 0;
 
   return (
-    <div>
+    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
       {isPetsEmpty ? (
         <h1>No Pets Found</h1>
       ) : (
